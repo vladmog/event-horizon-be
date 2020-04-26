5fb9040761f796ebe0fefddd21d8fea5c11deb00
vladmog <vladmog@gmail.com>
2020-04-26 13:48:45 -0700

    Added localhost to cors whitelist

--- api/server.js
+++ api/server.js
@@ -3,8 +3,12 @@ const apiRouter = require('../api-router.js');
 const server = express();
 const cors = require("cors");
 
+let whitelist = [
+    "http://localhost:3000", 
+    "https://master.d2mghrfmlzseon.amplifyapp.com"
+]
 
-server.use(cors({ origin: 'https://master.d2mghrfmlzseon.amplifyapp.com/' }));
+server.use(cors({ origin: whitelist }));
 server.use(express.json());
 server.use('/api', apiRouter);
 
